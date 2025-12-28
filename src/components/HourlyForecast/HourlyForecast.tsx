74694c87d6d1038ee64932495d2b246d7e7c76da
Max <max@Mac.lan>
2025-12-27 19:22:25 -0500

    added imports update jsx and  icons for diferent weather conditions

--- src/components/HourlyForecast/HourlyForecast.tsx
+++ src/components/HourlyForecast/HourlyForecast.tsx
@@ -1,9 +1,12 @@
 import { convertTemp } from "../../utils/Convert";
+import "./HourlyForecast.css";
+import { getWeatherIcon } from "../../utils/weatherIcons";
 
 type HourlyForecastProps = {
   hourlyForecast: {
     time: Date[];
     temperature: number[];
+    weather_code: number[];
   };
   unit: "metric" | "imperial";
   timezone: string;
@@ -17,20 +20,30 @@ function HourlyForecast({
   return (
     <div className="weather__hourly">
       <h3>Hourly forecast</h3>
-      {hourlyForecast.time.map((time: Date, index: number) => (
-        <div key={index}>
-          {time
-            .toLocaleTimeString("en-US", {
-              hour: "2-digit",
-              minute: "2-digit",
-              hour12: false,
-              timeZone: timezone,
-            })
-            .slice(0, 5)}
-          —{Math.round(convertTemp(hourlyForecast.temperature[index], unit))}°
-          {unit === "metric" ? "C" : "F"}
-        </div>
-      ))}
+      <div className="weather__hourly-list">
+        {hourlyForecast.time.map((time: Date, index: number) => (
+          <div className="weather__hourly-item" key={index}>
+            <div className="weather__hourly-time">
+              <img
+                src={getWeatherIcon(hourlyForecast.weather_code[index])}
+                alt="weather icon"
+              />
+              {time
+                .toLocaleTimeString("en-US", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                  hour12: false,
+                  timeZone: timezone,
+                })
+                .slice(0, 5)}
+            </div>
+            <div className="weather__hourly-temp">
+              {Math.round(convertTemp(hourlyForecast.temperature[index], unit))}
+              °{unit === "metric" ? "C" : "F"}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
